2df9b2acdbeaf74c83a9fb102004453cefd2a40d
GJZhang <37733294+GJZhang2866@users.noreply.github.com>
2024-08-05 17:44:55 +0800

    Update built_prototypes.py

--- built_prototypes.py
+++ built_prototypes.py
@@ -8,53 +8,68 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from   torch import nn
-
+from tqdm import tqdm
+# import matplotlib.pyplot as plt
 #
 # PArse user arguments.
 #
 def parse_args():
-    parser = argparse.ArgumentParser(description="Hyperspherical prototypes")
-    parser.add_argument('-c', dest="classes", default=66, type=int)
+    parser = argparse.ArgumentParser(description="Hyperspherical Multi-prototypes")
+    parser.add_argument('-c', dest="classes", default=81, type=int)
     parser.add_argument('-d', dest="dims", default=1024, type=int)
     parser.add_argument('-l', dest="learning_rate", default=0.1, type=float)
     parser.add_argument('-m', dest="momentum", default=0.9, type=float)
     parser.add_argument('-e', dest="epochs", default=10000, type=int,)
     parser.add_argument('-s', dest="seed", default=300, type=int)
-    parser.add_argument('-w', dest="wtvfile", default="", type=str)
+    parser.add_argument('-w', dest="wtvfile", default="semantics/wiki_bert_sem.npy", type=str) #wiki_bert_sem.npy
+    parser.add_argument('-per_num',dest="num_proto_per_type", default=3, type=int)
+    parser.add_argument('-hd',dest="wtv_dims", default=768, type=int)
     args = parser.parse_args()
     return args
 
-#
-# Compute the loss related to the prototypes.
-#
-def prototype_loss(prototypes):
-    # Dot product of normalized prototypes is cosine similarity.
-    product = torch.matmul(prototypes, prototypes.t()) + 1
-    # Remove diagnonal from loss.
-    product -= 2. * torch.diag(torch.diag(product))
-    # Minimize maximum cosine similarity.
-    loss = product.max(dim=1)[0]
-    return loss.mean(), product.max()
 
-#
+def prototype_loss(prototypes,mask):
+    
+    intra_class_similarities = torch.matmul(prototypes, prototypes.transpose(1, 2)) # [c,n,h ] [c,h,n]= [c,n,n]
+    intra_class_min_similarities = intra_class_similarities.min(dim=-1)[0]
+    intra_class_similarity_loss = (1 - intra_class_min_similarities).mean() 
+   
+    product = torch.matmul(prototypes.view(-1,args.dims),prototypes.view(-1,args.dims).t()) + 1
+    product -= 2. * torch.diag(torch.diag(product))
+    product[mask.bool()] = -2.0 
+    inter_class_similarity = product.max(dim=1)[0].mean()
+
+    loss = intra_class_similarity_loss + inter_class_similarity
+    return loss
+
+# #
 # Compute the semantic relation loss.
 #
-def prototype_loss_sem(prototypes, triplets):
-    product = torch.matmul(prototypes, prototypes.t()) + 1
-    product -= 2. * torch.diag(torch.diag(product))
-    loss1 = -product[triplets[:,0], triplets[:,1]]
-    loss2 = product[triplets[:,2], triplets[:,3]]
-    return loss1.mean() + loss2.mean(), product.max()
+def prototype_loss_sem(wtvv,triplets,prototypes):
+    
+    cos_sem_wtvv = torch.cosine_similarity(wtvv.unsqueeze(1), wtvv.unsqueeze(0), dim=2)
+    cos_porto_wtvv = torch.cosine_similarity(prototypes.view(-1,args.dims).unsqueeze(1), prototypes.view(-1,args.dims).unsqueeze(0), dim=-1)
+   
+    cos_sem_ij = cos_sem_wtvv[triplets[:, 0], triplets[:, 1]]
+    cos_sem_ik = cos_sem_wtvv[triplets[:, 0], triplets[:, 3]]
 
-#
-# Main entry point of the script.
-#
+    cos_proto_ij = cos_porto_wtvv[triplets[:, 0], triplets[:, 1]]
+    cos_proto_ik = cos_porto_wtvv[triplets[:, 0], triplets[:, 3]]
+
+    oijk = cos_proto_ij - cos_proto_ik
+    Sijk_true = torch.where(cos_sem_ij >= cos_sem_ik, torch.tensor(1.0).to(device), torch.tensor(0.0).to(device))
+    Sijk = torch.exp(oijk) / (1 + torch.exp(oijk))
+    loss = -Sijk_true * torch.log(Sijk) - (1 - Sijk_true) * torch.log(1 - Sijk)
+    
+       
+    return loss.mean() 
+
+
 if __name__ == "__main__":
-    # Parse user arguments.
     args = parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     device = torch.device("cuda")
-    kwargs = {'num_workers': 64, 'pin_memory': True}
+
 
     # Set seed.
     seed = args.seed
@@ -64,22 +79,34 @@ if __name__ == "__main__":
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-    # Initialize prototypes and optimizer.
-   
-    use_wtv = False
-    
     # Initialize prototypes.
-    prototypes = torch.randn(args.classes, args.dims)
-    prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=1))
+    prototypes = torch.randn(args.classes,args.num_proto_per_type, args.dims).to(device)
+    prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=-1))
+    # Initialize prototypes and optimizer.
+    if os.path.exists(args.wtvfile):
+        use_wtv = True
+        wtvv = np.load(args.wtvfile)
+        wtvv = torch.from_numpy(wtvv).to(device)
+        wtvv = wtvv.unsqueeze(1).repeat(1, args.num_proto_per_type, 1).view(-1,args.wtv_dims)
+        triplets = [[i,j,i,k] for i in tqdm(range(args.classes)) for j in range(args.classes) for k in range(args.classes) if i != j != k != i]
+        print("Trans 2 numpy.array...............")
+        triplets = np.array(triplets).astype(int)
+    else:
+        use_wtv = False
+
+    # Initialize prototypes.
     optimizer = optim.SGD([prototypes], lr=args.learning_rate, \
             momentum=args.momentum)
-
+    mask = torch.zeros(args.classes *args.num_proto_per_type, args.classes*args.num_proto_per_type)
+    for i in range(0, args.classes*args.num_proto_per_type, args.num_proto_per_type):
+           mask[i:i+args.num_proto_per_type,i:i+args.num_proto_per_type] = 1
+    losses = []
     # Optimize for separation.
-    for i in range(args.epochs):
+    for i in tqdm(range(args.epochs)):
         # Compute loss.
-        loss1, sep = prototype_loss(prototypes)
+        loss1 = prototype_loss(prototypes,mask)
         if use_wtv:
-            loss2 = prototype_loss_sem(prototypes, triplets)
+            loss2 = prototype_loss_sem(wtvv,triplets,prototypes)
             loss = loss1 + loss2
         else:
             loss = loss1
@@ -87,13 +114,13 @@ if __name__ == "__main__":
         loss.backward()
         optimizer.step()
         # Renormalize prototypes.
-        prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=1))
+        prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=-1))
         optimizer = optim.SGD([prototypes], lr=args.learning_rate, \
                 momentum=args.momentum)
-       
+        
+        print(str(args.epochs) +''+ str(loss)),
         sys.stdout.flush()
-    print
     
     # Store result.
-    np.save(args.resdir + "prototypes-%dd-%dc.npy" %(args.dims, args.classes), \
-            prototypes.data.numpy())
+    np.save(args.resdir + "prototypes-{}d-{}c_mutil{}_proto_sem_test".format(args.dims, args.classes,args.num_proto_per_type), \
+            prototypes.data.cpu().numpy())
